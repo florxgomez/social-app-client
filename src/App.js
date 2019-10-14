fa22bf25a626aa2b02c027e2a1ff784a08d41116
Flor <florxgomez@gmail.com>
2019-10-14 18:41:57 -0300

    post card details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //Components
 import NavBar from "./components/NavBar";
 
@@ -10,20 +11,43 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signUp from "./pages/signUp";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#c483de",
+      main: "#b665d6",
+      dark: "#7f4695",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#83deda",
+      main: "#65d6d1",
+      dark: "#469592",
+      contrastText: "#fff"
+      ,
+    },
+    typography: {
+      useNextVariants: true
+    }
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signUp} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signUp} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+
+import Grid from "@material-ui/core/Grid";
+import Post from '../components/Post';
 
 export class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-            </div>
-        )
-    }
+  state = {
+    posts: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ posts: res.data });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <Post post={post} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <div>
+        <Grid container spacing={10}>
+          <Grid item sm={8} xs={12}>
+            {recentPostsMarkup}
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <p>Profile...</p>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Home;
